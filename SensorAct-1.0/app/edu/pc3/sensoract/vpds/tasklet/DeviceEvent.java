e3ceed0bbf45c19b11087c700038bcc55a120e08
Manaswi Saha <manaswis@iiitd.ac.in>
2013-04-26 21:39:55 +0530

    Modified mutiple log file setup

--- SensorAct-1.0/app/edu/pc3/sensoract/vpds/tasklet/DeviceEvent.java
+++ SensorAct-1.0/app/edu/pc3/sensoract/vpds/tasklet/DeviceEvent.java
@@ -42,7 +42,7 @@ public class DeviceEvent extends Observable {
 		DeviceId deviceId = new DeviceId(ws.secretkey, ws.data.dname,
 				ws.data.sname, ws.data.sid);
 		
-		uploadLog.info("notifyWaveSegmentArrived.. DeviceId "
+		uploadLog.info(System.currentTimeMillis()/1000 + " :: notifyWaveSegmentArrived.. DeviceId "
 				+ deviceId.toString());
 
 		ArrayList<DeviceEventListener> listListener = mapListeners.get(deviceId
